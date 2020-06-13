34b6b0cb246f86d6745b10ceb7ff584c1ff690ae
Rahul Chaudhary <rahul300chaudhary400@gmail.com>
2020-06-13 11:48:19 -0400

    Bug Fix: Server must not start until database is connected

--- app.js
+++ app.js
@@ -1,63 +1,68 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const Lead = require('./models/lead');
-const indexRouter = require('./routes/index');
-const versionRouter = require('./routes/api/version');
-const comingSoonLeadRouter = require('./routes/api/coming-soon/lead');
+const express = require('express')
+const mongoose = require('mongoose')
+const Lead = require('./models/lead')
+const indexRouter = require('./routes/index')
+const versionRouter = require('./routes/api/version')
+const comingSoonLeadRouter = require('./routes/api/coming-soon/lead')
 
-const app = express();
+const app = express()
 
 // Database connection.
 const dbHost = process.env.DATABASE_HOST || 'localhost'
 const dbPort = process.env.DATABASE_PORT || 27017
 const databaseUrl = `mongodb://${dbHost}:${dbPort}/stock-adviser`
-mongoose.connect(databaseUrl, {useNewUrlParser: true, useUnifiedTopology: true});
 
-// Database events.
-const db = mongoose.connection;
-db.on('error', (error) => { // On error.
-  console.log(error);
-  process.exit(1)
-});
-db.on('open', async () => { // On init.
-  console.log('Server connected to database.');
-
-  const lead = await Lead.findOne({message: "Hang tight while we build this awesome feature."});
-  if (!lead) {
-    console.log('Seeding database.');
-
-    const messages = [
-      "Hang tight while we build this awesome feature.",
-      "Hold your horses, this thing ain't built yet.",
-      "Look! Look! More awesome features are on their way."
-    ];
-    messages.forEach((message) => {
-      const lead = new Lead({message});
-      lead.save((error) => {
-        if (error) {
-          console.log('Error seeding database!', error);
-          process.exit(1)
-        }
-      });
-    });
+mongoose.connect(databaseUrl, {useNewUrlParser: true, useUnifiedTopology: true}, (error, db) => {
+  if (error) {
+    console.log(error)
+    process.exit(1)
   }
-});
 
-// CORS policy.
-app.use(function(req, res, next) {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-  next();
-});
+  db.on('error', (error) => { // On error.
+    console.log(error)
+    process.exit(1)
+  })
 
-// Route registration.
-app.use('/', indexRouter);
-app.use('/api/version', versionRouter);
-app.use('/api/coming-soon/lead', comingSoonLeadRouter);
+  db.on('open', async () => { // On init.
+    console.log('Server connected to database.')
 
-const port = process.env.PORT || 8080;
-app.listen(port, () => {
-  console.log('Server running on port: ' + port);
+    const lead = await Lead.findOne({message: "Hang tight while we build this awesome feature."})
+    if (!lead) {
+      console.log('Seeding database.')
+  
+      const messages = [
+        "Hang tight while we build this awesome feature.",
+        "Hold your horses, this thing ain't built yet.",
+        "Look! Look! More awesome features are on their way."
+      ]
+      messages.forEach((message) => {
+        const lead = new Lead({message})
+        lead.save((error) => {
+          if (error) {
+            console.log('Error seeding database!', error)
+            process.exit(1)
+          }
+        })
+      })
+    }
+
+    // CORS policy.
+    app.use(function(req, res, next) {
+      res.header('Access-Control-Allow-Origin', '*');
+      res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+      next();
+    });
+
+    // Route registration.
+    app.use('/', indexRouter);
+    app.use('/api/version', versionRouter);
+    app.use('/api/coming-soon/lead', comingSoonLeadRouter);
+
+    const port = process.env.PORT || 8080;
+    app.listen(port, () => {
+      console.log('Server running on port: ' + port);
+    })
+  })
 })
 
 module.exports = app;
